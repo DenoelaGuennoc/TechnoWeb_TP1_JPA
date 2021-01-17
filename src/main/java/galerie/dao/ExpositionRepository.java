ff0610dbc16f5789cbefd4abaec6c297ff07e6c8
Denoëla <Denola@LAPTOP-965VQCJH>
2021-01-17 17:59:20 +0100

    Implémentation de la Query en SQL dans ExpositionRepository

--- src/main/java/galerie/dao/ExpositionRepository.java
+++ src/main/java/galerie/dao/ExpositionRepository.java
@@ -2,9 +2,26 @@ package galerie.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import galerie.entity.Exposition;
+import org.springframework.data.jpa.repository.Query;
 
 // This will be AUTO IMPLEMENTED by Spring 
 
 public interface ExpositionRepository extends JpaRepository<Exposition, Integer> {
+    
+    /**
+    * Calculer le chiffre d'affaires pour une exposition
+    * @param id la clé primaire de l'exposition
+    * @return le chiffre d'affaires de cette exposition
+    */
+    
+    @Query(
+        value = "SELECT SUM(prix_vente)"
+            + "FROM Transaction INNER JOIN Exposition"
+            + "ON(Transaction.lieu_de_vente_id = Exposition.id)"
+            + "WHERE Exposition.id = :id",
+        nativeQuery = true
+    )
+    
+    float chiffreAffairePour(Integer id);
     
 }
